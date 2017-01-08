7c9f08d10fee1ae5d5209f18d1b762702633e0fd
Daniel Kubec <niel@rtfm.cz>
2017-01-08 12:13:19 +0100

    Fixed repository name getter.

--- scripts/package/name.sh
+++ scripts/package/name.sh
@@ -1,1 +1,2 @@
-git remote -v | head -n1 | awk '{print $2}' | sed -e 's,.*:\(.*/\)\?,,' -e 's/\.git$//'
+#!/bin/sh
+echo "$(basename $(git remote -v | head -n1 | awk '{print $2}' | sed -e 's/\.git$//'))"
