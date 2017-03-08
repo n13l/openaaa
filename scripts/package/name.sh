c4a5a443745d6cb9621e5cf0366076b41d27b3dd
Daniel Kubec <niel@rtfm.cz>
2017-03-08 23:07:22 +0100

    Remove digits and dash from package name.

--- scripts/package/name.sh
+++ scripts/package/name.sh
@@ -3,5 +3,5 @@ package_dir=$(dirname $(dirname $(dirname $(readlink -f $0))))
 if [ -d "$package_dir/.git" ]; then
 	echo "$(basename $(git remote -v | head -n1 | awk '{print $2}' | sed -e 's/\.git$//'))"
 else
-	echo "$(basename $package_dir)"
+	echo "$(basename $package_dir)" | sed -e 's/[0-9]//g' -e 's/-//g'
 fi
