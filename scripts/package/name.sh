06fd42bf131a3b9d607dfd68316a5cd7b760a503
Daniel Kubec <niel@rtfm.cz>
2017-03-08 22:47:45 +0100

    Fixed package name script.

--- scripts/package/name.sh
+++ scripts/package/name.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-if [ -d .git ]; then
+package_dir=$(dirname $(dirname $(dirname $(readlink -f $0))))
+if [ -d "$package_dir/.git" ]; then
 	echo "$(basename $(git remote -v | head -n1 | awk '{print $2}' | sed -e 's/\.git$//'))"
 else
-	echo "$(basename $PWD)"
+	echo "$(basename $package_dir)"
 fi
