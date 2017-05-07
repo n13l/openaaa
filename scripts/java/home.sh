b8ed1959ef67ac585fec1b1d0a6baba8fae3121e
Daniel Kubec <niel@rtfm.cz>
2017-05-07 18:18:18 +0200

    Fixing jdk.

--- scripts/java/home.sh
+++ scripts/java/home.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
+if [ ! -z "$JAVA_HOME" ]; then 
+	PATH=$JAVA_HOME/bin:$PATH
+	exit 0; 
+fi
+
 if [ ! -z "$JDK_HOME" ]; then 
 	echo "$JDK_HOME"
 	exit 0; 
 fi
 
-if [[ "$1" == "linux" ]]; then 
+if [ "$1" == "linux" ]; then 
 	export JDK_HOME=$(readlink -f `which javac` | sed "s:/bin/javac::")
 	echo "$JDK_HOME"; exit 0;
 fi
 
-if [[ "$1" == "darwin" ]]; then
+if [ "$1" == "darwin" ]; then
 	export JDK_HOME=$( $(dirname $(readlink $(which javac)))/java_home )
 	echo "$JDK_HOME"; exit 0;
 fi
 
-if [[ "$1" == "win32" ]]; then
+if [ "$1" == "win32" ]; then
 	export JDK_HOME=$(dirname $(dirname $(which javac)))
 	echo "$JDK_HOME"; exit 0;
 fi
