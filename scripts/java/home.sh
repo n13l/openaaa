05696d59390de76f96da6008f88499553552fde7
Daniel Kubec <niel@rtfm.cz>
2017-05-07 20:04:37 +0200

    Fixup linux jdk build.

--- scripts/java/home.sh
+++ scripts/java/home.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ ! -z "$JAVA_HOME" ]; then 
+	if [ "$1" != "linux" ]; then
 	PATH=$JAVA_HOME/bin:$PATH
-	exit 0; 
+	exit 0;
+	fi
 fi
 
 if [ ! -z "$JDK_HOME" ]; then 
@@ -10,16 +12,19 @@ if [ ! -z "$JDK_HOME" ]; then
 fi
 
 if [ "$1" == "linux" ]; then 
-	export JDK_HOME=$(readlink -f `which javac` | sed "s:/bin/javac::")
-	echo "$JDK_HOME"; exit 0;
+	export JDK_HOME=$(readlink -f $(which javac) | sed "s:/bin/javac::")
+	echo "$JDK_HOME"; 
+	exit 0;
 fi
 
 if [ "$1" == "darwin" ]; then
 	export JDK_HOME=$( $(dirname $(readlink $(which javac)))/java_home )
-	echo "$JDK_HOME"; exit 0;
+	echo "$JDK_HOME"; 
+	exit 0;
 fi
 
 if [ "$1" == "win32" ]; then
 	export JDK_HOME=$(dirname $(dirname $(which javac)))
-	echo "$JDK_HOME"; exit 0;
+	echo "$JDK_HOME"; 
+	exit 0;
 fi
