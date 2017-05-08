f0f59a443ace650ea7bdc620482b1de98e4e75b8
Daniel Kubec <niel@rtfm.cz>
2017-05-08 15:56:44 +0200

    Add support for OPENAAA_SERVICE env.

--- test/ssl/tls-server.sh
+++ test/ssl/tls-server.sh
@@ -3,7 +3,6 @@ export OPENAAA_AUTHORITY=orange-d1.aducid.com
 export OPENAAA_PROTOCOL=aaa
 export OPENAAA_HANDLER=/usr/local/bin/aducid
 export OPENAAA_VERBOSITY=4
-export OPENAAA_SERVICE=10.16.60.142
-#export OPENAAA_SERVICE=aaa.rtfm.cz
+export OPENAAA_SERVICE=127.0.0.1
 export PATH=/opt/aaa/bin:$PATH
 sudo -E /opt/aaa/bin/openssl s_server -key ./test/ssl/key.pem -cert ./test/ssl/cert.pem -accept 4443 -www
