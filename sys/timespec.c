9c27b43a83ad462c219da402682b2018860d934c
Daniel Kubec <niel@rtfm.cz>
2017-12-30 06:22:51 +0100

    Add timespec utility functions.

---
+++ sys/timespec.c
@@ -0,0 +1,98 @@
+#include <sys/compiler.h>
+#include <sys/log.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdio.h>
+
+void
+timespec_to_timeval(struct timespec *ts, struct timeval *tv)
+{
+	tv->tv_sec  = ts->tv_sec;
+	tv->tv_usec = ts->tv_nsec / 1000;
+}
+
+void
+timespec_now(struct timespec *ts)
+{
+	struct timeval  tv;
+	gettimeofday(&tv, NULL);
+	ts->tv_sec  = tv.tv_sec;
+	ts->tv_nsec = tv.tv_usec * 1000;  
+}
+
+void
+timespec_add_ms(struct timespec *ts, long ms)
+{
+	int sec=ms / 1000;
+	ms=ms-sec * 1000;
+	ts->tv_nsec+=ms * 1000000;
+	ts->tv_sec+=ts->tv_nsec / 1000000000 + sec;
+	ts->tv_nsec=ts->tv_nsec % 1000000000;
+}
+
+void
+timespec_add_ns(struct timespec *ts, long ns)
+{
+	int sec = ns / 1000000000;
+	ns=ns - sec * 1000000000;
+	ts->tv_nsec += ns;
+	ts->tv_sec  += ts->tv_nsec / 1000000000 + sec;
+	ts->tv_nsec  = ts->tv_nsec % 1000000000;
+}
+
+void
+timeval_set(struct timeval *tv, double d)
+{
+	long l = d * 1000000;
+	tv->tv_sec = l / 1000000;
+	tv->tv_usec = l - tv->tv_sec * 1000000;
+}
+
+void
+timespec_adj(struct timespec *ts, double d)
+{
+	long sec = (int)d;
+	long ns = (d - sec) * 1000000000;
+
+	while (ns < 0) {
+		ns += 1000000000;
+		sec--;
+	}
+
+	ts->tv_nsec += ns;
+	ts->tv_sec  += ts->tv_nsec / 1000000000 + sec;
+	ts->tv_nsec  = ts->tv_nsec % 1000000000;
+}
+
+int
+timespec_cmp(struct timespec *a, struct timespec *b)
+{
+	if (a->tv_sec!=b->tv_sec)
+		return a->tv_sec-b->tv_sec;
+
+	return a->tv_nsec-b->tv_nsec;
+}
+
+void
+timespec_sub(struct timespec *a, struct timespec *b)
+{
+	a->tv_nsec = a->tv_nsec - b->tv_nsec;
+	if (a->tv_nsec < 0) {
+		a->tv_nsec += 1000000000;
+		a->tv_sec--;
+	}
+
+	a->tv_sec = a->tv_sec - b->tv_sec;
+}
+
+int
+timespec_milliseconds(struct timespec *ts) 
+{
+	return ts->tv_sec * 1000 + ts->tv_nsec / 1000000;
+}
+
+void
+timespec_show(struct timespec *ts)
+{
+	info("%li.%09li", ts->tv_sec, ts->tv_nsec);
+}
---
+++ sys/timespec.h
@@ -0,0 +1,58 @@
+/*
+ * The MIT License (MIT)
+ *                               Copyright (c) 2015 Daniel Kubec <niel@rtfm.cz> 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"),to deal 
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#ifndef __SYS_TIMESPEC_H__
+#define __SYS_TIMESPEC_H__
+
+void
+timespec_to_timeval(struct timespec *ts, struct timeval *tv)
+
+void
+timespec_now(struct timespec *ts);
+
+void
+timespec_add_ms(struct timespec *ts, long ms);
+
+void
+timespec_add_ns(struct timespec *ts, long ns);
+
+void
+timeval_set(struct timeval *tv, double d);
+
+void
+timespec_adj(struct timespec *ts, double d);
+
+int
+timespec_cmp(struct timespec *a, struct timespec *b);
+
+void
+timespec_sub(struct timespec *a, struct timespec *b);
+
+int
+timespec_milliseconds(struct timespec *ts) ;
+
+void
+timespec_show(struct timespec *ts);
+
+#endif/*__SYS_TIMESPEC_H__*/
