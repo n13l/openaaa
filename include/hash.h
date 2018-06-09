e7aecd7e5039f759c9f93c563788a7b7a4d05e49
Daniel Kubec <niel@rtfm.cz>
2018-06-09 19:56:24 +0200

    Fixed (c) of William Lee Irwin

--- include/hash.h
+++ include/hash.h
@@ -1,3 +1,16 @@
+/* Fast hashing routine for ints, longs and pointers.
+ *    (C) 2002 William Lee Irwin III, IBM 
+ *
+ * Knuth recommends primes in approximately golden ratio to the maximum
+ * integer representable by a machine word for multiplicative hashing.
+ * Chuck Lever verified the effectiveness of this technique:
+ * http://www.citi.umich.edu/techreports/reports/citi-tr-00-1.pdf
+ *
+ * These primes are chosen to be bit-sparse, that is operations on
+ * them can use shifts and additions instead of multiplications for
+ * machines where multiplications are slow.
+ */
+
 #ifndef __HASH_GENERIC_H__
 #define __HASH_GENERIC_H__
 
