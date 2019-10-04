12f337a79bbb14bf7e23f3e1e39a395fbcb078e3
Daniel Kubec <n13l@rtfm.cz>
2019-10-04 03:07:21 +0200

    Improve big load test.

--- test/aaa/cmd-big-test.sh
+++ test/aaa/cmd-big-test.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 #set -exu -o pipefail
-#set -exu
 
 for i in {1..50000}
 do
 
 sess_id="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 48 | head -n 1)"
 user_id="$(shuf -i 1-1000 -n 1)"
+auth_result="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 48 | head -n 1)"
 
 user_name=$(jq -r --arg user_id "$user_id" '.['$user_id'].first_name' ./test/aaa/users.json)
 user_lastname=$(jq -r --arg user_id "$user_id" '.['$user_id'].last_name' ./test/aaa/users.json)
@@ -40,8 +40,25 @@ res=$(printf "$req" | nc -4u -w1 $OPENAAA_SERVICE 8888)
 echo "res: $(echo "$res" | sed -e 'H;${x;s/\n/,/g;s/^,//;p;};d')"
 
 chk_user_name=$(echo "$res" | grep -o 'user.name:[^,]*' | sed -e 's/user.name://g')
-echo "compare (user_name) $chk_user_name $user_name"
 chk_sess_id=$(echo "$res" | grep -o 'sess.id:[^,]*' | sed -e 's/sess.id://g')
-echo "compare (sess_id) $chk_sess_id $sess_id"
+
+echo "update() sess.id: $sess_id"
+req="${op_commit}\nsess.id:${sess_id}\nauth.result:${auth_result}\n"
+#echo "req: $req"
+res=$(printf "$req" | nc -4u -w1 $OPENAAA_SERVICE 8888)
+echo "res: $(echo "$res" | sed -e 'H;${x;s/\n/,/g;s/^,//;p;};d')"
+
+chk_user_name=$(echo "$res" | grep -o 'user.name:[^,]*' | sed -e 's/user.name://g')
+chk_sess_id=$(echo "$res" | grep -o 'sess.id:[^,]*' | sed -e 's/sess.id://g')
+
+if [[ (-n "$res") && ("$chk_user_name" != "$user_name") ]]; then
+	  echo "user.name mistmatch $chk_user_name $user_name"
+	  exit1
+fi
+
+if [[ (-n "$res") && ("$chk_sess_id" != "$sess_id") ]]; then
+	  echo "user.name mistmatch $chk_sess_id $sess_id"
+	  exit 1
+fi
 
 done
